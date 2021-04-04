9182b483f735b439681794792b5dbdd783a6ef7b
Pavlo Kozlov <kozlov.pavel.s@gmail.com>
2021-04-04 20:19:37 +0200

    Add footer section for Card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,14 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './Card.scss';
 
-const Card = ({title, children}) => (
+/**
+ * Card component with title, body and footer divided by horizontal lines.
+ */
+const Card = ({ title, children, footerContent }) => (
     <div className="card">
         <h3 className="card--title">{title}</h3>
-        <hr/>
+        <hr />
         <div className="card--body">
             {children}
         </div>
+        <hr />
+        <div className="card--footer">
+            {footerContent}
+        </div>
     </div>
 );
+
+Card.propTypes = {
+    title: PropTypes.string.isRequired,
+    children: PropTypes.node,
+    footerContent: PropTypes.node,
+};
 
 export default Card;
